5f14be7604a7878280bcf1795a2cbed5c1e4445c
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-30 08:55:16 +0300

    Add project setup

--- elearning-zetech-clone/src/App.jsx
+++ elearning-zetech-clone/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../public/images/vite.svg'
-import './App.css'
+import React from 'react'
+import Hero from './Components/Hero/Hero'
+import Navigation from './Components/Navigation/Navigation'
+import Footer from './Components/Footer/Footer'
+// import reactLogo from './assets/react.svg'  -> src folder
+// import viteLogo from '../public/images/vite.svg' -> public folder (images that do not change suitable for build)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navigation />
+      <Hero />
+      <h1>Hello World!</h1>
+      <Footer />
     </>
   )
 }
-
 export default App
---
+++ elearning-zetech-clone/src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <nav>
+        <div className="container nav__container">
+            <p>This is a Navigation bar</p>
+        </div>
+    </nav>
+  )
+}
+
+export default Navigation
