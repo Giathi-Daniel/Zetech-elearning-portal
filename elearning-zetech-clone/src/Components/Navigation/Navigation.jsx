6e54c5d0424655129208645d716ff9678942e49d
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-02 21:06:14 +0300

    Create navigation component

--- elearning-zetech-clone/src/Components/Navigation/Navigation.jsx
+++ elearning-zetech-clone/src/Components/Navigation/Navigation.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { FaChevronDown } from "react-icons/fa";
 import './Navigation.css'
 
 const Navigation = () => {
   return (
     <nav>
         <div className="container nav__container">
-            <p>This is a Navigation bar</p>
+            <a href="#" className="logo"><img src="https://elearning.zetech.ac.ke/pluginfile.php/1/theme_edutor/logo/1716463041/Portrait%20Logo%20282C%20%281%29.png" alt="" /></a>
+            <ul className="nav_list">
+              <li><a href="#">Course Catalogue</a></li>
+              <li><a href="#">News and Announcement</a></li>
+              <li><a href="#">Teaching & Exams Timetables</a></li>
+              <li><a href="#">Results/Passlists</a></li>
+              <li>
+                <a href="#">Leaner Support 
+                  <FaChevronDown />
+                </a>
+              </li>
+              <li>
+                <a href="#">More
+                  <FaChevronDown />
+                </a>
+              </li>
+            </ul>
+            <div className="nav_social">
+              <a href="#">Login</a>
+            </div>
+            <div className="menu_btns">
+
+            </div>
         </div>
     </nav>
   )
