32511a60bb2febb5af147b3dba97486175ab0d09
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-03 11:16:45 +0300

    Update hero section

--- elearning-zetech-clone/src/Components/Hero/Hero.jsx
+++ elearning-zetech-clone/src/Components/Hero/Hero.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <section>
-      Hero Section
+    <section className="hero__container">
+      <div className="hero_section-user">
+        <img src="" alt="" />
+      </div>
+      <div className="hero_section-desc">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          doloribus sit dolore suscipit quasi unde similique aperiam? Ab,
+          cupiditate distinctio architecto sapiente itaque eum inventore nulla
+          alias cum quas consequuntur?
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
