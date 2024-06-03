52dc5b79d61e8224f466523b6054ef0cc00d88f8
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-03 11:40:29 +0300

    Add CTA component

--- elearning-zetech-clone/src/Components/Cta/Cta.jsx
+++ elearning-zetech-clone/src/Components/Cta/Cta.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import './Cta.css'
+import "./Cta.css";
 
 const Cta = () => {
   return (
     <>
       <section className="container action__container">
-          <div className="call_to_action-text">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-              esse.
-            </p>
-          </div>
-          <div className="call_to_action-input">
-            <input type="email" name="email" id="emailInput" />
-            <button type="submit">Call to action</button>
-          </div>
+        <div className="call_to_action-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            esse.
+          </p>
+        </div>
+        <div className="call_to_action-input">
+          <input type="email" name="email" id="emailInput" />
+          <button type="submit">Call to action</button>
+        </div>
       </section>
     </>
   );
