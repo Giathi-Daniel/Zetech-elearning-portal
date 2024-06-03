168f2237844146d3a17dc0ca4ebf4e93a27a7240
Daniel-Giathi <giathidaniel252@gmail.com>
2024-06-03 11:08:19 +0300

    Update navBar component

--- elearning-zetech-clone/src/Components/Navigation/Navigation.jsx
+++ elearning-zetech-clone/src/Components/Navigation/Navigation.jsx
@@ -6,21 +6,19 @@ const Navigation = () => {
   return (
     <nav>
         <div className="container nav__container">
-            {/* <a href="#" className="logo"><img src="https://elearning.zetech.ac.ke/pluginfile.php/1/theme_edutor/logo/1716463041/Portrait%20Logo%20282C%20%281%29.png" alt="" /></a> */}
+            <a href="#" className="logo"><img src="https://elearning.zetech.ac.ke/pluginfile.php/1/theme_edutor/logo/1716463041/Portrait%20Logo%20282C%20%281%29.png" alt="" /></a>
             <ul className="nav_list">
               <li><a href="#">Course Catalogue</a></li>
               <li><a href="#">News and Announcement</a></li>
               <li><a href="#">Teaching & Exams Timetables</a></li>
               <li><a href="#">Results/Passlists</a></li>
               <li>
-                <a href="#">Leaner Support 
-                  <FaChevronDown />
-                </a>
+                <a href="#">Leaner Support</a>
+                <FaChevronDown className='icon'/>
               </li>
               <li>
-                <a href="#">More
-                  <FaChevronDown />
-                </a>
+                <a href="#">More</a>
+                <FaChevronDown className='icon'/>
               </li>
             </ul>
             <div className="nav_social">
